ff3f0c48d2362daffa70c89870238e8d1af5a7b0
moghit-eou <moghit.elouaad1@gmail.com>
2025-07-30 21:54:06 +0100

    pushed

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__ , static_folder = 'static' , template_folder = 'templates') 
+
+
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Welcome to the Flask app!"})
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
